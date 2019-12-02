610fc15c4bc546e1c9c037d0f98aea07714e0385
Joseph Szul <JosephSzul@gmail.com>
2019-12-01 23:47:47 -0600

    Add project files.

---
+++ restClientCsharp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace restClientCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRequest = new System.Windows.Forms.TextBox();
+            this.txtresponse = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtRequest
+            // 
+            this.txtRequest.Location = new System.Drawing.Point(89, 62);
+            this.txtRequest.Name = "txtRequest";
+            this.txtRequest.Size = new System.Drawing.Size(370, 26);
+            this.txtRequest.TabIndex = 7;
+            // 
+            // txtresponse
+            // 
+            this.txtresponse.Location = new System.Drawing.Point(89, 94);
+            this.txtresponse.Multiline = true;
+            this.txtresponse.Name = "txtresponse";
+            this.txtresponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtresponse.Size = new System.Drawing.Size(500, 288);
+            this.txtresponse.TabIndex = 6;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(480, 59);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(109, 29);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Go!";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(26, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Response";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 428);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.txtresponse);
+            this.Controls.Add(this.txtRequest);
+            this.Name = "Form1";
+            this.Text = "c# Rest Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRequest;
+        private System.Windows.Forms.TextBox txtresponse;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ restClientCsharp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restClientCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        { 
+        RESTClient rClient = new RESTClient();
+        rClient.endPoint = txtRequest.Text;
+        debugOutput("RESTClient Object created.");
+
+        string strJSON = string.Empty;
+
+        strJSON = rClient.makeRequest();
+
+        debugOutput(strJSON);
+    }
+        private void debugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText + Environment.NewLine);
+                txtresponse.Text = txtresponse.Text + strDebugText + Environment.NewLine;
+                txtresponse.SelectionStart = txtresponse.TextLength;
+                txtresponse.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message, ToString() + Environment.NewLine);
+            }
+        }    
+    }
+}
